929d04347fc278fee3cfc8704cba446f5215accb
JonathanNyman <jonazan020@gmail.com>
2020-04-02 11:37:18 +0300

    Merging

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-console.log("Hejsan från utvecklare 2");
+console.log("Hejsan från utvecklare 1");
 
 function add (a, b) {
 	return a + b;
-console.log("Hejsan Från utvecklar 1");
 }
+
 function hello1() {
 console.log("Hello World")
 }
